2c452d4b615122ac18c31fb953b7596a41be4943
MVishukumar <beingvishu@gmail.com>
2020-10-22 01:22:05 +0530

    Progress...

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,7 +3,7 @@ console.log('Client JS loaded..........');
 const socket = io();
 
 socket.on('countUpdated', (count) => {
-    console.log('countUpdated event:' , count);
+	console.log('countUpdated event:', count);
 });
 
 // document.querySelector('#increment').addEventListener('click', () => {
@@ -20,58 +20,62 @@ const $messages = document.querySelector('#messages');
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
-const locationMessageTemplete = document.querySelector('#location-message-template').innerHTML;
+const locationMessageTemplete = document.querySelector(
+	'#location-message-template'
+).innerHTML;
 
 socket.on('message', (message) => {
-    console.log(message);
-    const html = Mustache.render(messageTemplate, {
-        message: message.text,
-        createdAt: moment(message.created).format('h:mm a')
-    });
-    $messages.insertAdjacentHTML('beforeend', html);
+	console.log(message);
+	const html = Mustache.render(messageTemplate, {
+		message: message.text,
+		createdAt: moment(message.created).format('h:mm a'),
+	});
+	$messages.insertAdjacentHTML('beforeend', html);
 });
 
 socket.on('locationMessage', (message) => {
-    console.log(message);
-    const html = Mustache.render(locationMessageTemplete, {
-        url: message.url,
-        createdAt: moment(message.createdAt).format('h:mm a')
-    });
-    $messages.insertAdjacentHTML('beforeend', html);
+	console.log(message);
+	const html = Mustache.render(locationMessageTemplete, {
+		url: message.url,
+		createdAt: moment(message.createdAt).format('h:mm a'),
+	});
+	$messages.insertAdjacentHTML('beforeend', html);
 });
 
-
 $chatForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+	event.preventDefault();
 
-    $chatFormSubmitButton.setAttribute('disabled', 'disabled');
+	$chatFormSubmitButton.setAttribute('disabled', 'disabled');
 
-    //const message = document.querySelector('#message').value;
-    const message = event.target.message.value;
+	//const message = document.querySelector('#message').value;
+	const message = event.target.message.value;
 
-    socket.emit('sendMessage', message, (callbackMessage) => {
-        $chatFormSubmitButton.removeAttribute('disabled')
-        $chatFormInputMessage.value = '';
-        $chatFormInputMessage.focus();
-        console.log(callbackMessage);
-    });
-})
+	socket.emit('sendMessage', message, (callbackMessage) => {
+		$chatFormSubmitButton.removeAttribute('disabled');
+		$chatFormInputMessage.value = '';
+		$chatFormInputMessage.focus();
+		console.log(callbackMessage);
+	});
+});
 
 document.querySelector('#sendLocation').addEventListener('click', () => {
-    if(!navigator.geolocation) {
-        return alert('Geolocation not available');
-    }
+	if (!navigator.geolocation) {
+		return alert('Geolocation not available');
+	}
 
-    navigator.geolocation.getCurrentPosition((position) => {
-        $sendLocationButton.setAttribute('disabled', 'disabled');
-        console.log(position);
+	navigator.geolocation.getCurrentPosition((position) => {
+		$sendLocationButton.setAttribute('disabled', 'disabled');
+		console.log(position);
 
-        console.log(position.coords.latitude, position.coords.longitude);
+		console.log(position.coords.latitude, position.coords.longitude);
 
-        socket.emit('sendLocation', {lat: position.coords.latitude, long: position.coords.longitude}, (ackServer) => {
-            $sendLocationButton.removeAttribute('disabled');
-            console.log(ackServer);
-        });
-
-    });
+		socket.emit(
+			'sendLocation',
+			{ lat: position.coords.latitude, long: position.coords.longitude },
+			(ackServer) => {
+				$sendLocationButton.removeAttribute('disabled');
+				console.log(ackServer);
+			}
+		);
+	});
 });
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const express = require('express');
 const socketio = require('socket.io');
 const { emit } = require('process');
 
-const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const {
+	generateMessage,
+	generateLocationMessage,
+} = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -17,29 +20,37 @@ app.use(express.static(publicDirectoryPath));
 
 let count = 0;
 io.on('connection', (socket) => {
-    console.log('New client connected');
+	console.log('New client connected');
 
-    socket.emit('message', generateMessage('Welcome!'));
+	socket.emit('message', generateMessage('Welcome!'));
 
-    // socket.broadcast.emit will emit message to all other clients except self
-    socket.broadcast.emit('message', generateMessage('A new user joined chat room'));
+	// socket.broadcast.emit will emit message to all other clients except self
+	socket.broadcast.emit(
+		'message',
+		generateMessage('A new user joined chat room')
+	);
 
-    socket.on('sendMessage', (message, callback) => {
-        console.log(message);
-        io.emit('message',generateMessage(message));
-        callback(':Delivered');
-    });
+	socket.on('sendMessage', (message, callback) => {
+		console.log(message);
+		io.emit('message', generateMessage(message));
+		callback(':Delivered');
+	});
 
-    socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has disconnected'));
-    });
+	socket.on('disconnect', () => {
+		io.emit('message', generateMessage('A user has disconnected'));
+	});
 
-    socket.on('sendLocation', (location, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.lat},${location.long}`));
-        callback(':Location shared successfully');
-    })
-})
+	socket.on('sendLocation', (location, callback) => {
+		io.emit(
+			'locationMessage',
+			generateLocationMessage(
+				`https://google.com/maps?q=${location.lat},${location.long}`
+			)
+		);
+		callback(':Location shared successfully');
+	});
+});
 
 server.listen(port, () => {
-    console.log('Node app started at port:', port);
-})
+	console.log('Node app started at port:', port);
+});
