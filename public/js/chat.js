96e3196607d2e6388eae707ffc4fc1d3568ce253
MVishukumar <beingvishu@gmail.com>
2020-10-22 00:54:20 +0530

    Some progress

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,8 +6,72 @@ socket.on('countUpdated', (count) => {
     console.log('countUpdated event:' , count);
 });
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked');
-    socket.emit('increment')
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked');
+//     socket.emit('increment')
+// });
+
+// Elements
+const $chatForm = document.querySelector('#chatForm');
+const $chatFormInputMessage = $chatForm.querySelector('#message');
+const $chatFormSubmitButton = $chatForm.querySelector('#submit');
+const $sendLocationButton = document.querySelector('#sendLocation');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplete = document.querySelector('#location-message-template').innerHTML;
+
+socket.on('message', (message) => {
+    console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.created).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
 });
 
+socket.on('locationMessage', (message) => {
+    console.log(message);
+    const html = Mustache.render(locationMessageTemplete, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
+
+
+$chatForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    $chatFormSubmitButton.setAttribute('disabled', 'disabled');
+
+    //const message = document.querySelector('#message').value;
+    const message = event.target.message.value;
+
+    socket.emit('sendMessage', message, (callbackMessage) => {
+        $chatFormSubmitButton.removeAttribute('disabled')
+        $chatFormInputMessage.value = '';
+        $chatFormInputMessage.focus();
+        console.log(callbackMessage);
+    });
+})
+
+document.querySelector('#sendLocation').addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not available');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        $sendLocationButton.setAttribute('disabled', 'disabled');
+        console.log(position);
+
+        console.log(position.coords.latitude, position.coords.longitude);
+
+        socket.emit('sendLocation', {lat: position.coords.latitude, long: position.coords.longitude}, (ackServer) => {
+            $sendLocationButton.removeAttribute('disabled');
+            console.log(ackServer);
+        });
+
+    });
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const { emit } = require('process');
+
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,14 +19,25 @@ let count = 0;
 io.on('connection', (socket) => {
     console.log('New client connected');
 
-    socket.emit('countUpdated', count);
+    socket.emit('message', generateMessage('Welcome!'));
 
-    socket.on('increment', () => {
-        count++;
-        //socket.emit('countUpdated', count)
+    // socket.broadcast.emit will emit message to all other clients except self
+    socket.broadcast.emit('message', generateMessage('A new user joined chat room'));
 
-        io.emit('countUpdated', count);
+    socket.on('sendMessage', (message, callback) => {
+        console.log(message);
+        io.emit('message',generateMessage(message));
+        callback(':Delivered');
     });
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has disconnected'));
+    });
+
+    socket.on('sendLocation', (location, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.lat},${location.long}`));
+        callback(':Location shared successfully');
+    })
 })
 
 server.listen(port, () => {
