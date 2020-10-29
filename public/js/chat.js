b6009b665084e2c404874ef0fcd422e26c9ddb14
MVishukumar <beingvishu@gmail.com>
2020-10-30 00:58:43 +0530

    Almost finished

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,7 +15,7 @@ socket.on('countUpdated', (count) => {
 const $chatForm = document.querySelector('#chatForm');
 const $chatFormInputMessage = $chatForm.querySelector('#message');
 const $chatFormSubmitButton = $chatForm.querySelector('#submit');
-const $sendLocationButton = document.querySelector('#sendLocation');
+//const $sendLocationButton = document.querySelector('#sendLocation');
 const $messages = document.querySelector('#messages');
 
 // Templates
@@ -24,6 +24,7 @@ const locationMessageTemplete = document.querySelector(
 	'#location-message-template'
 ).innerHTML;
 const sidebarTemplete = document.querySelector('#sidebar-template').innerHTML;
+const headerTemplete = document.querySelector('#header-template').innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, {
@@ -58,7 +59,7 @@ socket.on('message', (message) => {
 	const html = Mustache.render(messageTemplate, {
 		username: message.username,
 		message: message.text,
-		createdAt: moment(message.created).format('h:mm a'),
+		createdAt: moment(message.created).format('LT'),
 	});
 	$messages.insertAdjacentHTML('beforeend', html);
 	//autoscroll();
@@ -83,6 +84,11 @@ socket.on('roomData', ({ room, users }) => {
 
 	document.querySelector('#sidebar').innerHTML = html;
 	//$messages.insertAdjacentHTML('beforeend', html);
+
+	const html2 = Mustache.render(headerTemplete, {
+		room,
+	});
+	document.querySelector('#roomHeader').innerHTML = html2;
 });
 
 $chatForm.addEventListener('submit', (event) => {
@@ -101,27 +107,27 @@ $chatForm.addEventListener('submit', (event) => {
 	});
 });
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
-	if (!navigator.geolocation) {
-		return alert('Geolocation not available');
-	}
+// document.querySelector('#sendLocation').addEventListener('click', () => {
+// 	if (!navigator.geolocation) {
+// 		return alert('Geolocation not available');
+// 	}
 
-	navigator.geolocation.getCurrentPosition((position) => {
-		$sendLocationButton.setAttribute('disabled', 'disabled');
-		console.log(position);
+// 	navigator.geolocation.getCurrentPosition((position) => {
+// 		$sendLocationButton.setAttribute('disabled', 'disabled');
+// 		console.log(position);
 
-		console.log(position.coords.latitude, position.coords.longitude);
+// 		console.log(position.coords.latitude, position.coords.longitude);
 
-		socket.emit(
-			'sendLocation',
-			{ lat: position.coords.latitude, long: position.coords.longitude },
-			(ackServer) => {
-				$sendLocationButton.removeAttribute('disabled');
-				console.log(ackServer);
-			}
-		);
-	});
-});
+// 		socket.emit(
+// 			'sendLocation',
+// 			{ lat: position.coords.latitude, long: position.coords.longitude },
+// 			(ackServer) => {
+// 				$sendLocationButton.removeAttribute('disabled');
+// 				console.log(ackServer);
+// 			}
+// 		);
+// 	});
+// });
 
 socket.emit('join', { username, room }, (callback) => {
 	if (callback) {
